cd0acf2bfecbf92372cec9237848093b28b5cd3b
Ying Huang <huangyingw@gmail.com>
2020-07-27 09:07:59 -0700

    n

--- run.sh
+++ run.sh
@@ -3,4 +3,4 @@ SCRIPT=$(realpath "$0")
 SCRIPTPATH=$(dirname "$SCRIPT")
 cd "$SCRIPTPATH"
 
-./youtube_dl/__main__.py -f 'bestvideo+bestaudio' -o '%(title)s.%(ext)s' --restrict-filenames https://youtu.be/XfoYk_Z5AkI 
+./youtube_dl/__main__.py -f 'bestvideo+bestaudio' -o '%(title)s.%(ext)s' --restrict-filenames --write-sub https://youtu.be/XfoYk_Z5AkI 
